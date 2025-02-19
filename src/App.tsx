330342ebb20001f5837dea4f4ee170591609dec3
fortdev210 <sharp.blader2050@gmail.com>
2025-02-19 15:13:27 +0800

    connect wallets

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { WalletProvider } from "./hooks/WalletProvider"
+import { WalletList } from "./components/wallet-list/wallet-list"
+import { SelectedWallet } from "./components/selected-wallet/selected-wallet"
+import { WalletError } from "./components/wallet-error/wallet-error"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <WalletProvider>
+      <WalletList />
+      <hr />
+      <SelectedWallet />
+      <WalletError />
+    </WalletProvider>
   )
 }
 
---
+++ src/components/wallet-error/wallet-error.tsx
@@ -0,0 +1,20 @@
+import { useWalletProvider } from "../../hooks/useWalletProvider"
+import styles from "./wallet-error.module.css"
+
+export const WalletError = () => {
+  const { errorMessage, clearError } = useWalletProvider()
+  const isError = !!errorMessage
+
+  return (
+    <div
+      className={styles.walletError}
+      style={isError ? { backgroundColor: "brown" } : {}}
+    >
+      {isError && (
+        <div onClick={clearError}>
+          <strong>Error:</strong> {errorMessage}
+        </div>
+      )}
+    </div>
+  )
+}
